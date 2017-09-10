d5ef69cd5a94bd35d02e52e6fb5eecc6ac1094a2
Roxane Marcilhacy <roxane.marcilhacy@gmail.com>
2017-09-10 23:24:27 +0200

    change require relative

--- config.rb
+++ config.rb
@@ -74,7 +74,7 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
   #
-  require_relative ".lib/build_cleaner"
+  require_relative "./lib/build_cleaner"
   activate :build_cleaner
 end
 
